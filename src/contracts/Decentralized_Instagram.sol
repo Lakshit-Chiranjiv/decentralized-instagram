55e7e79e15c321d858d8ca439578cc3781038050
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-10 16:08:49 +0530

    func created for uploadPost

--- src/contracts/Decentralized_Instagram.sol
+++ src/contracts/Decentralized_Instagram.sol
@@ -30,5 +30,14 @@ contract Decentralized_Instagram{
     mapping(address => User) public UserAddresses;
     mapping(string => uint) public UserNames;
     mapping(uint => Post) public Posts;
-    
+
+    function uploadPost(string memory _postHash,string memory _caption) public{
+        require(bytes(_postHash).length > 0,"Post Hash not provided");
+        require(bytes(_caption).length > 0,"Caption not given");
+        require(msg.sender != address(0),"Not a valid user address");
+        postCount++;
+        Posts[postCount] = Post(postCount,_postHash,_caption,0,0,payable(msg.sender));
+        UserAddresses[msg.sender].postHashesIds.push(postCount);
+    }
+
 }
