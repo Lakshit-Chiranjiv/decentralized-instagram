f05e9668671a7875be85dd8ebfbd7604f91c0557
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-10 16:05:54 +0530

    user struct created

--- src/contracts/Decentralized_Instagram.sol
+++ src/contracts/Decentralized_Instagram.sol
@@ -5,4 +5,15 @@ pragma solidity >=0.7.0 <0.9.0;
 contract Decentralized_Instagram{
     uint public postCount = 0;
     uint public userCount = 0;
+
+    struct User{
+        uint userId;
+        address payable userAddress;
+        string username;
+        string avatarHash;
+        string userBio;
+        uint tipsReceived;
+        uint tipsGiven;
+        uint[] postHashesIds;
+    }
 }
