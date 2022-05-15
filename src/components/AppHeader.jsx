906fc5b3db65e4e5909441b5b22eeacf9db4d52a
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-15 14:09:42 +0530

    app header component

---
+++ src/components/AppHeader.jsx
@@ -0,0 +1,18 @@
+import { red } from '@ant-design/colors'
+import { Layout, Row, Typography } from 'antd'
+import React from 'react'
+
+const { Header } = Layout;
+const { Title } = Typography;
+
+const AppHeader = () => {
+  return (
+    <Header style={{ backgroundColor: red[2],height: 'auto', padding: 30 }}>
+        <Row align='middle' justify='center'>
+        <Title style={{ color: 'white' }}>Decentralized Instagram</Title>
+        </Row>
+    </Header>
+  )
+}
+
+export default AppHeader
