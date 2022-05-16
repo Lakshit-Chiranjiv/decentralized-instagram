b1ff8b91430fce7e6446313fb47a735368ddcd1d
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-16 21:52:46 +0530

    added some test data

---
+++ src/testData.js
@@ -0,0 +1,44 @@
+export const testData = [
+    {
+        imgUrl: './../../assets/img1.jpg',
+        likes: 54,
+        tips: 3,
+        user: 'George',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+    {
+        imgUrl: './../../assets/img2.jpg',
+        likes: 73,
+        tips: 1,
+        user: 'Anna',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+    {
+        imgUrl: './../../assets/img3.jpg',
+        likes: 21,
+        tips: 0,
+        user: 'Dave',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+    {
+        imgUrl: './../../assets/img4.jpg',
+        likes: 124,
+        tips: 7,
+        user: 'Daisy',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+    {
+        imgUrl: './../../assets/img5.jpg',
+        likes: 44,
+        tips: 2,
+        user: 'Bob',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+    {
+        imgUrl: './../../assets/img6.jpg',
+        likes: 33,
+        tips: 0,
+        user: 'Alice',
+        caption: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci, repudiandae.'
+    },
+]
