36015371900002c7cba848aacd61606e78bc02b5
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-16 21:52:27 +0530

    PostCard component created

---
+++ src/components/PostCard.jsx
@@ -0,0 +1,49 @@
+import React from 'react'
+import { Card, Avatar, Image, Typography, Divider } from 'antd';
+import { HeartOutlined,DollarCircleOutlined } from '@ant-design/icons';
+
+const { Meta } = Card;
+const { Text } = Typography; 
+
+const PostCard = ({data: {imgUrl,likes,user,caption,tips}}) => (
+    <>
+        <Card
+            style={{ width: '100%',padding: 20,borderRadius: 10 }}
+            cover={
+            <Image
+                width={300}
+                height={300}
+                alt="example"
+                src={imgUrl}
+            />
+            }
+            // actions={[
+            //     <HeartOutlined key="like" />,
+            //     <DollarCircleOutlined key="tip"/>
+            // ]}
+        >
+            <Meta
+            avatar={<Avatar src="https://joeschmoe.io/api/v1/random" />}
+            title={user}
+            description={caption}
+            />
+
+            <Divider/>
+
+            <div style={{display: 'flex',justifyContent: 'space-around',marginTop: 20,fontSize: 18}}>
+                <div style={{cursor: 'pointer'}}>
+                    <HeartOutlined key="like" style={{ color: 'red', margin: '0 10px'}}/>
+                    <Text>{likes}</Text>
+                </div>
+                <Divider type='vertical' style={{height: 30}}/>
+                <div style={{cursor: 'pointer'}}>
+                    <DollarCircleOutlined key="tip" style={{ color: 'green', margin: '0 10px'}}/>
+                    <Text>{tips} Eth</Text>
+                </div>
+            </div>
+        </Card>
+
+    </>
+);
+
+export default PostCard
