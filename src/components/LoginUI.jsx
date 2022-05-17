d6109c225a7dfbf86d08693e5ee0a334867bb646
Lakshit-Chiranjiv <luckyhacker017@gmail.com>
2022-05-17 22:18:29 +0530

    completed the rough UI for sider component

--- src/components/LoginUI.jsx
+++ src/components/LoginUI.jsx
@@ -2,6 +2,8 @@ import { Image, Layout, Typography } from 'antd'
 import React from 'react'
 import { testData } from './../testData.js'
 import PostCard from './PostCard.jsx';
+import SiderItem from './SiderItem.jsx';
+import { UserOutlined,PictureOutlined,HomeOutlined } from '@ant-design/icons';
 
 const { Paragraph,Title,Text } = Typography;
 const { Header, Footer, Sider, Content } = Layout;
@@ -11,7 +13,11 @@ console.log(testData)
 const LoginUI = () => {
   return (
         <Layout>
-            <Sider style={{ color: 'white', padding: '20px' }}>Sider</Sider>
+            <Sider style={{ color: 'white', padding: '20px' }}>
+                <SiderItem icon={<HomeOutlined style={{fontSize: 40}}/>} name='Home'/>
+                <SiderItem icon={<PictureOutlined style={{fontSize: 40}}/>} name='Post a Pic'/>
+                <SiderItem icon={<UserOutlined style={{fontSize: 40}}/>} name='Profile'/>
+            </Sider>
             <Content style={{display: 'grid',gridTemplateColumns: 'repeat(2, 1fr)'}}>
                 {
                     testData.map((data,i) => (
